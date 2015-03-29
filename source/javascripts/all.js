cbfb638e45b47e1df24150aed4344fa2dc0bca8a
Natalie Kozlowski <kozlow47@gmail.com>
2015-03-29 19:17:14 -0400

    NK homepage done working on detail view

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
   var year = new Date().getFullYear();
-  jQuery(".year").text(year);
+  $(".year").text(year);
+
+  $(".toTop").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  $(".holder").mouseover(function() {
+    $(this).children("span").fadeIn(300);
+  });
+
+  $(".holder").mouseleave(function() {
+    $(this).children("span").fadeOut(300);
+  });
 });// document.ready
