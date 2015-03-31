da79ce296557ad7c8d27d978d7fe44795840fcf6
Natalie Kozlowski <kozlow47@gmail.com>
2015-03-30 20:17:23 -0400

    NK website complete and deployed for now

---
+++ build2/projects/javascripts/all.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var year = new Date().getFullYear();
+  $(".year").text(year);
+
+  $(".toTop").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  $(".holder").mouseover(function() {
+    $(this).children("span").fadeIn(300);
+  });
+
+  $(".holder").mouseleave(function() {
+    $(this).children("span").fadeOut(300);
+  });
+});// document.ready
+;
