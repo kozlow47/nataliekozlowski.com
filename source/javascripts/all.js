e3d294f0857a12323da6549b6b1019ddbbc265ff
Natalie Kozlowski <kozlow47@gmail.com>
2015-09-16 23:56:43 -0400

    NK add a few new projects, include google tracking code, implement mixitup jquery for sorting and have filter navigation for sorting projects now

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+  // Instantiate MixItUp:
+  $(".previewContainer").mixItUp();
+
   var year = new Date().getFullYear();
   $(".year").text(year);
 
   $(".toTop").click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
+  });
+
+  $(".filterNav li").click(function() {
+    $(".filterNav li").removeClass("active");
+    $(this).addClass("active");
   });
 });// document.ready
